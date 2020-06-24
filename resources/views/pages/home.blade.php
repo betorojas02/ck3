df98ba465780a16b2ed113902e6fe29e04e4d3c0
Laura <lblancov@unbosque.edu.co>
2020-06-23 21:30:27 -0500

    Parte de Laura

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,25 +3,63 @@
     <!-- Page Preloder -->
 
     <!-- Header Section Begin -->
+    <!-- PARTE DE LAURA -->
+    <section>
+    <div class="row">
+            <div class="col-12 col-md-12 col-sm-12 col-xs-12">
+                <centering>
+                <section>
+                <!-- The video -->
+                        <video autoplay muted loop id="myVideo" width="1040" height="860" controls>
+                        <source src="img/baNNER CANISCATUS.mp4" type="video/mp4">
+                        </video>
+
+                        
+                </section>
+                <centering>
+            </div>
+
+        </div>
+    </section>
+    
+     <section class="section bg-light py-5  bottom-slant-gray">
+      <div class="container">
+        <div class="row align-items-center">
+
+          <div class="col-lg-6" alt="Image" class="img-fluid">
+          <div style="position:relative">
+            <img src="img/rect. negro foto.png" width="300" hspace="10" height="400" vspace="0.1" />
+            <DIV STYLE="position:absolute; top:50px; left:10px; visibility:visible z-index:1">
+            <IMG height="300" SRC="img/IMG_0168.jpg" width="300">
+            </div>
+            </div>
+          </div>
+          <div class="col-lg-5 ml-auto">
+            <div class="text-left heading-wrap">
+              <h2><font face="arial">¿</font><font face="Kohne Makina">QUIENES SOMOS</font><font face="arial">?</font></h2>
+            </div>
+            <!-- <h3 class="mb-4">Welcome To Our Restaurant</h3> -->
+            <font face="Liberation Sans Regular">  <p align="justify"> <br> <strong>CANISCATUS S.A.S. Es una empresa creada con el objetivo principal de brindar un servicio de calidad y calidez
+            para tu mascota, teniendo en cuenta las características y necesidades de la misma, Así mismo, las instalaciones
+            dispuestas para ello, están adaptadas y acondicionadas pensado en el bienestar animal... </strong></p>
+            <div style="text-align: right;width:420px">
+            <p><a class="boton_personalizado" href="https://vinkula.com">Conócenos más!</a></font></p>
+            </div>
+          </div>
+
+            
+          
+        </div>
+      </div>
+    </section>
+
 
     <!-- Header End -->
 
     <!-- Footer Section End -->
 
 
-        <div class="row">
-            <div class="col-12 col-md-12 col-sm-12 col-xs-12">
-                <section>
-                <!-- Hero Slider End -->
-                <!-- Features Section Begin -->
-
-                    Nico lindo
-
-                </section>
-            </div>
-
-        </div>
-
+      
 
 
 
